6a3cb28b6a623982198ad5b8fdd63ef50f2d64da
Praise Tompane <praisetompane@icloud.com>
2024-03-21 18:03:41 +0200

    fix(culture_models): add logging for clarity
    --date=0.day.ago

--- src/app_corporate_culture_measurer/train/train.py
+++ src/app_corporate_culture_measurer/train/train.py
@@ -12,14 +12,14 @@ from train import culture_models
 
 
 def train():
-    # train and apply a phrase model to detect 2-word phrases ----------------
-    
+    logging.log(logging.INFO, "training bigram model from corpus")
     culture_models.train_ngram_model(
         input_path=Path(
             global_options.DATA_FOLDER, "processed", "unigram", "documents.txt"
         ),
         model_path=Path(global_options.MODEL_FOLDER, "phrases", "bigram.mod"),
     )
+    logging.log(logging.INFO, "completed training bigram model from corpus")
     culture_models.apply_ngram_model(
         input_path=Path(
             global_options.DATA_FOLDER, "processed", "unigram", "documents.txt"
@@ -32,7 +32,7 @@ def train():
         threshold=global_options.PHRASE_THRESHOLD,
     )
 
-    # train and apply a phrase model to detect 3-word phrases ----------------
+    logging.log(logging.INFO, "training trigram model from bigrammed corpus")
     culture_models.train_ngram_model(
         input_path=Path(
             global_options.DATA_FOLDER, "processed", "bigram", "documents.txt"
@@ -50,10 +50,10 @@ def train():
         scoring="original_scorer",
         threshold=global_options.PHRASE_THRESHOLD,
     )
+    logging.log(logging.INFO, "completed training trigram model from bigrammed corpus")
 
-    # train the word2vec model ----------------
-    print(datetime.datetime.now())
-    print("Training w2v model...")
+
+    logging.log(logging.INFO, "training trigram w2v model from trigrammed corpus")
     culture_models.train_w2v_model(
         input_path=Path(
             global_options.DATA_FOLDER, "processed", "trigram", "documents.txt"
@@ -64,3 +64,4 @@ def train():
         workers=global_options.N_CORES,
         epochs=global_options.W2V_ITER,
     )
+    logging.log(logging.INFO, "completed training trigram w2v model from trigrammed corpus")
