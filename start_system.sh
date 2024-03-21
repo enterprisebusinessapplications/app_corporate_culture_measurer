7f15ed540967f735bd6a16144dacbe1336cd0cb7
Praise Tompane <praisetompane@icloud.com>
2024-03-21 20:35:42 +0200

    fix(root): clean up and project admin
    --date=0.day.ago

--- start_system.sh
+++ start_system.sh
@@ -2,19 +2,3 @@ echo "Starting up the sytem."
 cd ./src/app_corporate_culture_measurer/
 python app_corporate_culture_measurer.py
 echo "done"
-
-# echo "clean and train"
-#     python clean_and_train.py
-# echo "done"
-
-# echo "create culture dictionary"
-#     python create_dict.py
-# echo "done"
-
-# echo "score"
-#     python score.py
-# echo "done"
-
-# echo "aggregate firms"
-#     python aggregate_firms.py
-# echo "done"
